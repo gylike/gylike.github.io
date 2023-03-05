15214e0d015ec740b267a2fba402ae7b7246b7f0
Orange <sunzc1012@163.com>
2023-03-05 16:47:24 +0800

    Site updated: 2023-03-05 16:47:24

--- js/fomal.js
+++ js/fomal.js
@@ -639,132 +639,132 @@ function randomPost() {
 //----------------------------------------------------------------
 
 /* 小猫咪 start */
-if (document.body.clientWidth > 992) {
-  function getBasicInfo() {
-    /* 窗口高度 */
-    var ViewH = $(window).height();
-    /* document高度 */
-    var DocH = $("body")[0].scrollHeight;
-    /* 滚动的高度 */
-    var ScrollTop = $(window).scrollTop();
-    /* 可滚动的高度 */
-    var S_V = DocH - ViewH;
-    var Band_H = ScrollTop / (DocH - ViewH) * 100;
-    return {
-      ViewH: ViewH,
-      DocH: DocH,
-      ScrollTop: ScrollTop,
-      Band_H: Band_H,
-      S_V: S_V
-    }
-  };
-  function show(basicInfo) {
-    if (basicInfo.ScrollTop > 0.001) {
-      $(".neko").css('display', 'block');
-    } else {
-      $(".neko").css('display', 'none');
-    }
-  }
-  (function ($) {
-    $.fn.nekoScroll = function (option) {
-      var defaultSetting = {
-        top: '0',
-        scroWidth: 6 + 'px',
-        z_index: 9999,
-        zoom: 0.9,
-        borderRadius: 5 + 'px',
-        right: 55.6 + 'px',
-        nekoImg: "https://bu.dusays.com/2022/07/20/62d812db74be9.png",
-        hoverMsg: "春天啦~",
-        color: "var(--theme-color)",
-        during: 500,
-        blog_body: "body",
-      };
-      var setting = $.extend(defaultSetting, option);
-      var getThis = this.prop("className") !== "" ? "." + this.prop("className") : this.prop("id") !== "" ? "#" +
-        this.prop("id") : this.prop("nodeName");
-      if ($(".neko").length == 0) {
-        this.after("<div class=\"neko\" id=" + setting.nekoname + " data-msg=\"" + setting.hoverMsg + "\"></div>");
-      }
-      let basicInfo = getBasicInfo();
-      $(getThis)
-        .css({
-          'position': 'fixed',
-          'width': setting.scroWidth,
-          'top': setting.top,
-          'height': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 + 'px',
-          'z-index': setting.z_index,
-          'background-color': setting.bgcolor,
-          "border-radius": setting.borderRadius,
-          'right': setting.right,
-          'background-image': 'url(' + setting.scImg + ')',
-          'background-image': '-webkit-linear-gradient(45deg, rgba(255, 255, 255, 0.1) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.1) 50%, rgba(255, 255, 255, 0.1) 75%, transparent 75%, transparent)', 'border-radius': '2em',
-          'background-size': 'contain'
-        });
-      $("#" + setting.nekoname)
-        .css({
-          'position': 'fixed',
-          'top': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 - 50 + 'px',
-          'z-index': setting.z_index * 10,
-          'right': setting.right,
-          'background-image': 'url(' + setting.nekoImg + ')',
-        });
-      show(getBasicInfo());
-      $(window)
-        .scroll(function () {
-          let basicInfo = getBasicInfo();
-          show(basicInfo);
-          $(getThis)
-            .css({
-              'position': 'fixed',
-              'width': setting.scroWidth,
-              'top': setting.top,
-              'height': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 + 'px',
-              'z-index': setting.z_index,
-              'background-color': setting.bgcolor,
-              "border-radius": setting.borderRadius,
-              'right': setting.right,
-              'background-image': 'url(' + setting.scImg + ')',
-              'background-image': '-webkit-linear-gradient(45deg, rgba(255, 255, 255, 0.1) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.1) 50%, rgba(255, 255, 255, 0.1) 75%, transparent 75%, transparent)', 'border-radius': '2em',
-              'background-size': 'contain'
-            });
-          $("#" + setting.nekoname)
-            .css({
-              'position': 'fixed',
-              'top': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 - 50 + 'px',
-              'z-index': setting.z_index * 10,
-              'right': setting.right,
-              'background-image': 'url(' + setting.nekoImg + ')',
-            });
-          if (basicInfo.ScrollTop == basicInfo.S_V) {
-            $("#" + setting.nekoname)
-              .addClass("showMsg")
-          } else {
-            $("#" + setting.nekoname)
-              .removeClass("showMsg");
-            $("#" + setting.nekoname)
-              .attr("data-msg", setting.hoverMsg);
-          }
-        });
-      this.click(function (e) {
-        btf.scrollToDest(0, 500)
-      });
-      $("#" + setting.nekoname)
-        .click(function () {
-          btf.scrollToDest(0, 500)
-        });
-      return this;
-    }
-  })(jQuery);
+// if (document.body.clientWidth > 992) {
+//   function getBasicInfo() {
+//     /* 窗口高度 */
+//     var ViewH = $(window).height();
+//     /* document高度 */
+//     var DocH = $("body")[0].scrollHeight;
+//     /* 滚动的高度 */
+//     var ScrollTop = $(window).scrollTop();
+//     /* 可滚动的高度 */
+//     var S_V = DocH - ViewH;
+//     var Band_H = ScrollTop / (DocH - ViewH) * 100;
+//     return {
+//       ViewH: ViewH,
+//       DocH: DocH,
+//       ScrollTop: ScrollTop,
+//       Band_H: Band_H,
+//       S_V: S_V
+//     }
+//   };
+//   function show(basicInfo) {
+//     if (basicInfo.ScrollTop > 0.001) {
+//       $(".neko").css('display', 'block');
+//     } else {
+//       $(".neko").css('display', 'none');
+//     }
+//   }
+//   (function ($) {
+//     $.fn.nekoScroll = function (option) {
+//       var defaultSetting = {
+//         top: '0',
+//         scroWidth: 6 + 'px',
+//         z_index: 9999,
+//         zoom: 0.9,
+//         borderRadius: 5 + 'px',
+//         right: 55.6 + 'px',
+//         nekoImg: "https://bu.dusays.com/2022/07/20/62d812db74be9.png",
+//         hoverMsg: "春天啦~",
+//         color: "var(--theme-color)",
+//         during: 500,
+//         blog_body: "body",
+//       };
+//       var setting = $.extend(defaultSetting, option);
+//       var getThis = this.prop("className") !== "" ? "." + this.prop("className") : this.prop("id") !== "" ? "#" +
+//         this.prop("id") : this.prop("nodeName");
+//       if ($(".neko").length == 0) {
+//         this.after("<div class=\"neko\" id=" + setting.nekoname + " data-msg=\"" + setting.hoverMsg + "\"></div>");
+//       }
+//       let basicInfo = getBasicInfo();
+//       $(getThis)
+//         .css({
+//           'position': 'fixed',
+//           'width': setting.scroWidth,
+//           'top': setting.top,
+//           'height': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 + 'px',
+//           'z-index': setting.z_index,
+//           'background-color': setting.bgcolor,
+//           "border-radius": setting.borderRadius,
+//           'right': setting.right,
+//           'background-image': 'url(' + setting.scImg + ')',
+//           'background-image': '-webkit-linear-gradient(45deg, rgba(255, 255, 255, 0.1) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.1) 50%, rgba(255, 255, 255, 0.1) 75%, transparent 75%, transparent)', 'border-radius': '2em',
+//           'background-size': 'contain'
+//         });
+//       $("#" + setting.nekoname)
+//         .css({
+//           'position': 'fixed',
+//           'top': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 - 50 + 'px',
+//           'z-index': setting.z_index * 10,
+//           'right': setting.right,
+//           'background-image': 'url(' + setting.nekoImg + ')',
+//         });
+//       show(getBasicInfo());
+//       $(window)
+//         .scroll(function () {
+//           let basicInfo = getBasicInfo();
+//           show(basicInfo);
+//           $(getThis)
+//             .css({
+//               'position': 'fixed',
+//               'width': setting.scroWidth,
+//               'top': setting.top,
+//               'height': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 + 'px',
+//               'z-index': setting.z_index,
+//               'background-color': setting.bgcolor,
+//               "border-radius": setting.borderRadius,
+//               'right': setting.right,
+//               'background-image': 'url(' + setting.scImg + ')',
+//               'background-image': '-webkit-linear-gradient(45deg, rgba(255, 255, 255, 0.1) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.1) 50%, rgba(255, 255, 255, 0.1) 75%, transparent 75%, transparent)', 'border-radius': '2em',
+//               'background-size': 'contain'
+//             });
+//           $("#" + setting.nekoname)
+//             .css({
+//               'position': 'fixed',
+//               'top': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 - 50 + 'px',
+//               'z-index': setting.z_index * 10,
+//               'right': setting.right,
+//               'background-image': 'url(' + setting.nekoImg + ')',
+//             });
+//           if (basicInfo.ScrollTop == basicInfo.S_V) {
+//             $("#" + setting.nekoname)
+//               .addClass("showMsg")
+//           } else {
+//             $("#" + setting.nekoname)
+//               .removeClass("showMsg");
+//             $("#" + setting.nekoname)
+//               .attr("data-msg", setting.hoverMsg);
+//           }
+//         });
+//       this.click(function (e) {
+//         btf.scrollToDest(0, 500)
+//       });
+//       $("#" + setting.nekoname)
+//         .click(function () {
+//           btf.scrollToDest(0, 500)
+//         });
+//       return this;
+//     }
+//   })(jQuery);
 
-  $(document).ready(function () {
-    //部分自定义
-    $("#myscoll").nekoScroll({
-      bgcolor: 'rgb(0 0 0 / .5)', //背景颜色，没有绳子背景图片时有效
-      borderRadius: '2em',
-      zoom: 0.9
-    }
-    );
+//   $(document).ready(function () {
+//     //部分自定义
+//     $("#myscoll").nekoScroll({
+//       bgcolor: 'rgb(0 0 0 / .5)', //背景颜色，没有绳子背景图片时有效
+//       borderRadius: '2em',
+//       zoom: 0.9
+//     }
+//     );
     //自定义（去掉以下注释，并注释掉其他的查看效果）
     /*
     $("#myscoll").nekoScroll({
@@ -783,8 +783,8 @@ if (document.body.clientWidth > 992) {
         during:1200, //从顶部到底部滑动的时长
     });
     */
-  })
-}
+//   })
+// }
 
 /* 小猫咪 end */
 
